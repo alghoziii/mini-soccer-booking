4cba868eb3e77172b3ae9dfc76cf9bc017a1e22c
alghozii_zyy <shobarudin67@students.amikom.ac.id>
2025-09-20 20:55:50 +0700

    update payment service

--- field-service-main/constants/error/general.go
+++ field-service-main/constants/error/general.go
@@ -4,13 +4,15 @@ import "errors"
 
 var (
 	ErrInternalServerError = errors.New("internal server error")
-	ErrSQLError            = errors.New("database server failed to execute query")
-	ErrTooManyRequests     = errors.New("too many requests")
-	ErrUnauthorized        = errors.New("unauthorized")
-	ErrInvalidToken        = errors.New("invalid token")
-	ErrInvalidUploadFile   = errors.New("invalid upload file")
-	ErrSizeTooBig          = errors.New("size too big")
-	ErrForbidden           = errors.New("forbidden")
+= errors.New(
+"database server failed to execute query"
+)
+ErrTooManyRequests     = errors.New("too many requests")
+ErrUnauthorized = errors.New("unauthorized")
+ErrInvalidToken = errors.New("invalid token")
+ErrInvalidUploadFile = errors.New("invalid upload file")
+ErrSizeTooBig = errors.New("size too big")
+ErrForbidden = errors.New("forbidden")
 )
 
 var GeneralErrors = []error{
